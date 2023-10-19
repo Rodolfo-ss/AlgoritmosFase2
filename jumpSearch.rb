c796322349c74f41b74463cfc4adde23f9f0fbf6
Rodolfo  Campos <111758185+Rodolfo-ss@users.noreply.github.com>
2023-10-19 09:05:18 +0000

    jump

---
+++ jumpSearch.rb
@@ -0,0 +1,14 @@
+array = [2, 5, 23, 88, 21, 22, 45, 75, 12, 78]
+
+
+def jump_search(target, form)
+    counter = 0
+    while counter < array.length
+        if  array.counter == target
+            return counter
+        else
+            counter += 1
+        end 
+    end
+    return nil
+end
