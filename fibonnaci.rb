e57e62704212ee249d8ad324614bf7880fa2746e
Rodolfo  Campos <111758185+Rodolfo-ss@users.noreply.github.com>
2023-10-19 00:12:26 +0000

    fibonnaci

---
+++ fibonnaci.rb
@@ -0,0 +1,30 @@
+# def fibonacci (n)
+#     return n if (0..1).include? n 
+#     (fibonacci(n - 1) + fibonacci ( n - 2) )
+# end
+
+# # puts fibonacci(2)
+
+
+# def fib(n)
+#     n < 2 ? n: fib(n-1) + fib(n - 2)
+# end
+
+# puts fib
+
+
+def fibo(n)
+    curr = 0 
+    succ = 1
+    n.times do |i|
+        curr, succ = succ, curr + succ
+    end
+    return curr
+end 
+
+puts fibo(12)
+
+
+
+
+
