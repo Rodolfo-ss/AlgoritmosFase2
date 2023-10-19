5c09bd6849fb8f4ace58e72ec5b317c6585412bf
Rodolfo  Campos <111758185+Rodolfo-ss@users.noreply.github.com>
2023-10-19 00:00:37 +0000

    common

---
+++ common.rb
@@ -0,0 +1,22 @@
+common_here = "My word is wonderfull"
+
+puts  common_here 
+
+common_time = "My word is wonderfull"
+puts common_time 
+
+def character 
+    character = Hash.new
+    puts common_time
+end
+
+
+common_here.delete('c')
+common_time.match('word')
+
+
+puts common_time
+
+
+
+
