526bba9afdca0791dd03eda430c77c39f5c143b9
Rodolfo  Campos <111758185+Rodolfo-ss@users.noreply.github.com>
2023-10-22 01:23:53 +0000

    reorder

---
+++ reorderIndex.rb
@@ -0,0 +1,15 @@
+arr = [22, 3, 2, 6, 43, 88]
+arr_s = arr.sort
+
+puts arr.map{|e| arr_s.index(e)}
+
+
+
+arr2 = [22, 13, 52, 86, 143, 88]
+
+arr_s = arr.sort
+puts arr2.map{|e| arr_s.index(e)}
+
+
+
+
